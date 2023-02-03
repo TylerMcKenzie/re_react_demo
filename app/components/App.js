377b6ee47568d91a248ff507b96fff2f79ae9b76
tylermckenzie <tylrmckenz@gmail.com>
2023-02-02 19:14:04 -0600

    create intial app component

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                This is only a test.
+                </div>
+            )
+    }
+}
+
+module.exports = App;
